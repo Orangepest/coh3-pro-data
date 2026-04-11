93e1397fa38daab8a6afc1e973d61e9a4911ddf7
Shorgi Shorg <shorgi@lunarmerlin.net>
2026-04-11 14:15:00 +0900

    Save user position observations for 8 maps from Propagandacast videos
    User watched cast videos and identified physical position of one player
    per map. We have observations but slot 0/1 mapping requires user's gut
    feel for which side is favored (or finding cast .rec files in DB).

--- map_slot_labels.py
+++ map_slot_labels.py
@@ -16,7 +16,7 @@ MAP_SLOT_LABELS: dict[str, dict[int, str]] = {
     # Tushkan = slot 0 = south, Nicko = slot 1 = north
     "crossing_in_the_woods_2p": {0: "south", 1: "north"},
 
-    # === To be labeled (need a casted/known game) ===
+    # === To be labeled (need slot↔position mapping confirmed) ===
     # "langres_2p":              {},
     # "twin_beach_2p_mkii":      {},
     # "angoville_farms_2p":      {},
@@ -29,6 +29,24 @@ MAP_SLOT_LABELS: dict[str, dict[int, str]] = {
     # "tuscan_vineyard_2p":      {},
     # "cliff_crossing_2p":       {},
     # "villa_fiore_2p_mkii":     {},
+}
+
+
+# User observations from Propagandacast videos. These are physical positions
+# of specific players in specific cast games, but we don't yet know which slot
+# (0 or 1) those players occupied in those casts (the cast .rec files aren't
+# in our DB). To convert these to slot labels we need:
+#   1. The user's gut feel for which side is favored on each map, OR
+#   2. The cast .rec files added to our DB
+USER_OBSERVATIONS = {
+    "bologna_2p":          {"player": "Reakly",     "faction": "uk",   "position": "south", "video": "CBGDw9mzn7s"},
+    "djebel_2p":           {"player": "Nub",        "faction": "uk",   "position": "right", "video": "mIYZB63v7NE"},
+    "langres_2p":          {"player": "Rei",        "faction": "wehr", "position": "south", "video": "2_ZtvimPb0w"},
+    "angoville_farms_2p":  {"player": "Tushkan",    "faction": "wehr", "position": "south", "video": "PUtUsnQP4Ac"},
+    "twin_beach_2p_mkii":  {"player": "Barbarossa", "faction": "us",   "position": "south", "video": "HVFPW50gqrQ"},
+    "faymonville":         {"player": "Rei",        "faction": "dak",  "position": "north", "video": "PKvfTqQp2HU"},
+    "pachino_2p":          {"player": "RedxWings",  "faction": "us",   "position": "right", "video": "0a-6RarGRv0"},
+    "villa_fiore_2p_mkii": {"player": "FeriG",      "faction": "dak",  "position": "south", "video": "09m472rCano"},
 }
 
 
